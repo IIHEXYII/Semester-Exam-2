151e6f7ab5c935b0fbe787386720c7da8a22e734
HeXy <57751832+IIHEXYII@users.noreply.github.com>
2021-04-13 12:13:27 +0200

    Added: Nav  & auth in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthContext';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-   <h1>Hello World</h1>
+    <AuthProvider>
+    <Router>
+       <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import AuthContext from '../auth/AuthContext';
+
+function Nav() {
+  const [auth, setAuth] = useContext(AuthContext);
+
+  const history = useHistory();
+
+  function logout() {
+    setAuth(null);
+    history.push('/');
+  }
+
+  return (
+    <nav className="nav">
+      <Link className="nav__link" to='/'>Home</Link>
+      {auth ? (
+        <>
+          | <Link className="nav__link" to='/contact'>Contact</Link> 
+          <button className="nav__logout" onClick={logout}>Log out</button>
+        </>
+      ) : (
+        <Link className="nav__link" to='/login'>Login</Link>
+      )}
+    </nav>
+  );
+}
+
+export default Nav;
