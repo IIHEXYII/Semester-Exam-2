ac27f19971c3183b7a5b8a681a71edada7a66856
HeXy <57751832+IIHEXYII@users.noreply.github.com>
2021-04-14 11:36:14 +0200

    .env fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/sass/main.scss';
+import './assets/scss/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
