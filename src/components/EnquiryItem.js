2e556298e4c3fdaed1632bee2f32ec89aa936286
IIHEXYII <vetletessem@gmail.com>
2021-05-12 21:51:51 +0200

    Booking Submit done need title id & price

--- src/components/EnquiryItem.js
+++ src/components/EnquiryItem.js
@@ -3,6 +3,7 @@ const EnquiryItem = props => {
     const { id, first_name, last_name, email, people, comment, date_from, date_to } = props;
     return (
       <div className="admin-item" id={id}>
+        {/* <h2 className="admin-item__text">{title}</h2> */}
         <div className="admin-item__container">
         <h2 className="admin-item__title">{first_name}</h2>
         <h2 className="admin-item__title">{last_name}</h2>
@@ -14,11 +15,12 @@ const EnquiryItem = props => {
           <p className="admin-item__text"><BiCalendarPlus />From: {date_from}</p>
           <p className="admin-item__text"><BiCalendarMinus />To: {date_to}</p>
           </div>
+          {/* <p className="admin-item__text">{(date_to - date_from) * price}</p> */}
           <p className="admin-item__text">{comment}</p>
-         
         </div>
       </div>
     );
   };
+        //  {//////////////////////////* ADD TITLE AND PRICE TO STRAPI  *//////////////////////////////}
   
   export default EnquiryItem;
